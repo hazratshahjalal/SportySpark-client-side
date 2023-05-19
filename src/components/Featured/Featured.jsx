47a1ace975a3f4042088b45f75d7f911fe804bf8
hazratshahjalal <hazratshahjalal503@gmail.com>
2023-05-19 09:19:50 +0600

    featured section functionality update

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import toy1 from '../../assets/tennis.jpg';
+import toy2 from '../../assets/Transformer-Toy-Car.jpg';
+import toy3 from '../../assets/tennis1.jpg';
+import toy4 from '../../assets/tennis1.jpg';
+import toy5 from '../../assets/tennis1.jpg';
+import toy6 from '../../assets/tennis1.jpg';
+import toy7 from '../../assets/tennis1.jpg';
+import toy8 from '../../assets/tennis1.jpg';
+import toy9 from '../../assets/tennis1.jpg';
+import toy10 from '../../assets/tennis1.jpg';
+import '../Featured/Featured.css';
 
 const Featured = () => {
+  const toys = [toy10, toy9, toy8, toy7, toy6, toy5, toy4, toy3, toy2, toy1];
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
+
   return (
-    <div>
-      <h1 className="text-5xl">this is featured product section</h1>
+    <div className="overflow-hidden my-12">
+      <h1 className='text-5xl font-semibold text-center my-6'>Featured Products</h1>
+      <div className="flex w-full gap-20 space-x-20 animate-marquee justify-end">
+        {toys.map((toy, index) => (
+          <img
+            key={index}
+            src={toy}
+            alt={`Toy ${index + 1}`}
+            className={`w-50 h-40 transition-transform ${hoveredIndex === index ? 'scale-110' : ''}`}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
+          />
+        ))}
+      </div>
     </div>
   );
 };
